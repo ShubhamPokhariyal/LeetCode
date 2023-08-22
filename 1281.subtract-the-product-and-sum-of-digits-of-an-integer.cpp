cccfacd0651c0e6c33029ef640f7fc166f6fd260
ShubhamPokhariyal <shubhampokhariyal2@gmail.com>
2023-08-22 13:46:23 +0530

    Update 1281.subtract-the-product-and-sum-of-digits-of-an-integer.cpp

--- 1281.subtract-the-product-and-sum-of-digits-of-an-integer.cpp
+++ 1281.subtract-the-product-and-sum-of-digits-of-an-integer.cpp
@@ -8,7 +8,15 @@
 class Solution {
 public:
     int subtractProductAndSum(int n) {
-        
+        int sum=0,prod=1;
+        int digit;
+        while(n>0){
+            digit=n%10;
+            sum += digit;
+            prod *= digit;
+            n/=10;
+        }
+        return prod-sum;
     }
 };
 // @lc code=end
